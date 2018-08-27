a7f6043acbed9c64fc8e4e610f39150db0586321
DESKTOP-36NDODL\ppywm <ppywmjr@gmail.com>
2018-08-27 22:00:07 +0100

    ParameterValuesArray as interface

--- src/main/java/ApplicationUnderTest/Model/ParameterValuesArray.java
+++ src/main/java/ApplicationUnderTest/Model/ParameterValuesArray.java
@@ -1,34 +1,9 @@
 package ApplicationUnderTest.Model;
 
-import javafx.util.Pair;
-import lombok.Getter;
-import lombok.Setter;
+public interface ParameterValuesArray {
+    public Object getParameterValueByIndex(Integer integer);
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Map;
+    public Object getValueByCoordinates(Integer parameterIndex, Integer valueIndex);
 
-public class ParameterValuesArray {
-    @Getter
-    private ArrayList< Pair<String,ArrayList<String>> > parameterToValuesPairArray;
-
-    public ParameterValuesArray(ArrayList<Pair<String, ArrayList<String>>> parameterToValuesPairArray) {
-        this.parameterToValuesPairArray = parameterToValuesPairArray;
-    }
-
-    public String getParameterValueByIndex(Integer integer){
-        return parameterToValuesPairArray.get(integer).getKey();
-    }
-
-    public String getValueByCoordinates(Integer parameterIndex, Integer valueIndex){
-        return parameterToValuesPairArray.get(parameterIndex).getValue().get(valueIndex);
-    }
-
-    public Integer getNumberOfParameters(){
-        return parameterToValuesPairArray.size();
-    }
-
-    public Integer getNumberOfValuesFromParameterIndex(Integer parameterIndex){
-        return parameterToValuesPairArray.get(parameterIndex).getValue().size();
-    }
+    public Integer getNumberOfParameters();
 }
